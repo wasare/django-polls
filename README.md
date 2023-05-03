335af4a04a6d12d52d26c66ba5274f2f23978551
Wanderson Reis <wasare@gmail.com>
2023-05-03 09:23:22 -0300

    Primeira view index.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 "# django-polls" 
+
+Após clonar o repositório:
+
+No terminal:
+ 
+   python -m venv venv
+   .\venv\Scripts\activate.bat
+   pip install django
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+# View index ... carregada para alguma rota (caminho)
+def index(request):
+    return HttpResponse('Olá... seja bem vindo a enquete')
