45a9abc97cba47895dd52c86ca790fbbc589f52d
Wanderson Reis <wasare@gmail.com>
2023-05-17 09:18:17 -0300

    Configura banco de dados mysql

--- my_site/settings.py
+++ my_site/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+      'default': {
+                'ENGINE': 'django.db.backends.mysql',
+                'NAME': 'django_polls_db',
+                'USER': 'root',
+                'PASSWORD': '',
+                'HOST': 'localhost',
+                'PORT': '3306',
+      }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
