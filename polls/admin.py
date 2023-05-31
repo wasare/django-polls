a254c687675110b356e6fa065b3cf3cf6775d2c1
Wanderson Reis <wasare@gmail.com>
2023-05-31 09:09:46 -0300

    Inclui app polls na área administrativa.

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from polls.models import Question, Choice
+
+admin.site.register(Question)
+admin.site.register(Choice)
+
+# em caso de erro fatal no servidor
+# corrigir o erro
+# executar CTRL + C no terminal
+# executar o servidor novamente python manage.py runserver
