071e1fb1bf036848f6979f1becf9fa6d5c03dc41
Wanderson Reis <wasare@gmail.com>
2023-06-06 11:05:23 -0300

    View e rota que exibe uma pergunta pelo id.

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sobre/', views.sobre, name='sobre')
+    path('sobre/', views.sobre, name='sobre'),
+    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao')
 ]
 
 # .\venv\Scripts\activate.bat
--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
+from polls.models import Question, Choice
 
 # View index ... carregada para alguma rota (caminho)
 def index(request):
@@ -10,3 +10,12 @@ def index(request):
 
 def sobre(request):
     return HttpResponse('Este é um app de enquete!')
+
+def exibe_questao(request, question_id):
+    questao = Question.objects.get(id=question_id)
+    
+    if questao is not None:
+        # questao.question_text
+        return HttpResponse(questao.question_text)
+    
+    return HttpResponse('Não existe questão a exibir')
