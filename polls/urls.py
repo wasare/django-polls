231ce14404f9992631e904db8a60a99f5fccc9e3
Wanderson Reis <wasare@gmail.com>
2023-06-20 10:50:47 -0300

    Inclui página com as últimas perguntas.

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('sobre/', views.sobre, name='sobre'),
-    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao')
+    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao'),
+    path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
 ]
 
 # .\venv\Scripts\activate.bat
--- polls/views.py
+++ polls/views.py
@@ -19,3 +19,9 @@ def exibe_questao(request, question_id):
         return HttpResponse(questao.question_text)
     
     return HttpResponse('Não existe questão a exibir')
+
+
+def ultimas_perguntas(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/perguntas.html', context)
